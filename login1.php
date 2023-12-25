6ea2561bfcf01d4328d8222a0b5662f406579166
shreya singh <112194327+shreyasingh824@users.noreply.github.com>
2023-12-25 23:20:15 +0530

    Create login1.php

---
+++ login1.php
@@ -0,0 +1,33 @@
+<?php
+include('connection.php');
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+
+    $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1) {
+        // Start a session
+        session_start();
+
+        // Set a session variable
+        $_SESSION['username'] = $username;
+
+        // Set a cookie with the username (example: valid for 1 day)
+        setcookie('user_cookie', $username, time() + (86400 * 1), "/");
+
+        // Redirect to welcome.php
+        header("Location: welcome.php");
+        exit();
+    } else {
+        echo '<script>
+                alert("Login failed");
+                window.location.href = "index.php";
+              </script>';
+    }
+}
+?>
